d7d9d18558936f82252878157dd03cf22ea5b388
alber <albertolazarobelinchon@gmail.com>
2023-05-23 01:01:12 +0200

    Funcionalidad para ver peliculas

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const movieInput = document.getElementById("movieInput");
 
-//"2embed.to/embed/imdb/movie?id="
 function searchMovie(input) {
-    let api = `https://v3.sg.media-imdb.com/suggestion/x/${input}.json?includeVideos=1`;
+    const apiURL = `https://v3.sg.media-imdb.com/suggestion/x/${input}.json?includeVideos=1`;
+    fetch(apiURL).then((res) => res.json()).then((data) => {
+        let output = "";
+        data.d.forEach((movie) => {
+            output += `
+                <div class="card">
+                    <div class="card-img">
+                        <img src="${movie.i.imageUrl}" alt="${movie.l}">
+                    </div>
+                    <div class="card-info">
+                        <h3>${movie.l}</h3>
+                        <p>${movie.s}</p>
+                        <p>${movie.y}</p>
+                        <p>${movie.id}</p>
+                        <button onclick="showMovie('${movie.id}')">View Movie</button>
+                    </div>
+                </div>
+            `;
+        });
 
-    fetch(api).then((response) => {response.json}).then((data) => {console.log(data)});
+        document.getElementById("movies").innerHTML = output;
+    });
 }
-
 
 movieInput.addEventListener("keyup", (e) => {
     searchMovie(e.target.value);
 });
+
+function showMovie(id){
+    const apiURL = `https://2embed.to/embed/imdb/movie?id=${id}`;
+    fetch(apiURL).then((data) => {
+        let output = `
+            <div class="movie">
+                <iframe src="${data.url}" frameborder="0" allowfullscreen></iframe>
+            </div>
+        `;
+        console.log(data.url)
+        document.getElementById("movie").innerHTML = output;
+    }).catch((err) => {
+        console.log(err);
+    });
+}
